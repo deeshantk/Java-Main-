bdaf8fb973aee97de0de3b1104b070dcf52b5717
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2021-02-09 13:23:42 +0530

    Create Client.java

---
+++ Client-Server one way communication[two machines on same isp]/Client.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+public class Client{
+    public static void main(String[] args){
+        try{
+            Socket s = new Socket("192.168.1.102", 8080);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            s.close();
+        }
+        catch(Exception e){a
+            System.out.println(e);
+        }
+    }
+}
